0ac83c9aa948a23666382c8e436eed037f82ab75
Harsh Bhargava <68488593+bhargavah49@users.noreply.github.com>
2023-06-30 11:29:50 +0000

    Update 2 files

--- assignment-3/src/App.jsx
+++ assignment-3/src/App.jsx
@@ -1,15 +1,11 @@
 
 import './App.css'
 import CardListing from './components/CardListing'
-
 function App() {
-
-
   return (
     <div className='App'>
       <CardListing/>
     </div>
   )
 }
-
 export default App
